802c3897320d183cc3b0010300619d2af9cbeef0
FrankSchuch <187791840+FrankSchuch@users.noreply.github.com>
2026-01-27 20:28:45 +0000

    def

--- 1_.py
+++ 1_.py
@@ -1,3 +1,53 @@
+# function = A block of reusable code
+#            place () after the function name to invoke it
+
+# def happy_birthday(name, age):
+#     print(f"Happy Birthday to {name}!")
+#     print(f"You are {age} years old!")
+#     print("Happy Birthday to you!")
+#     print()
+
+# happy_birthday("Bro", 20)
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 40)
+# def display_invoice(username, amount, due_date):
+#     print(f"hello ({username})")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("BroCode", 42.50, "01/01")
+# display_invoice("JoeSchmoe", 67.67, "06/07")
+# display_invoice("AnthonyGooseMan", 41.41, "04/01")
+
+#return = statement used to end a function
+#         and send a result back to the caller
+
+# def add(x,y):
+#     z = x + y
+#     return z
+
+# def subtract(x,y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x,y):
+#     z = x - y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1,2))
+# print(multiply(1,2))
+# print(divide(1,2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+full_name = create_name("spongebob", "squarepants")
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
